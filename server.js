c0afa78206efce75667fd2249f5fcfa6a206d455
sumca252 <ismail.ali9315@gmail.com>
2022-09-03 23:04:37 +0200

    adds 404 handler

--- server.js
+++ server.js
@@ -26,6 +26,12 @@ app.use(express.urlencoded({ extended: true }));
 // routes
 app.use("/api/editor", require("./routes/editor.routes"));
 
+// 404 handler - must be last route handler
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Not found" });
+    next();
+});
+
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = server;
