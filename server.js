acf34dcabb45d1ddb9e47081ea443af7b8e62af8
sumca252 <ismail.ali9315@gmail.com>
2022-09-20 11:53:14 +0200

    adds socket.io

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
-require("dotenv").config();
-
 const app = express();
 
-const port = process.env.PORT || 3000;
+const httpServer = require("http").createServer(app);
+
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.sockets.on("connection", function (socket) {
+    console.log(`New client connected: ${socket.id}`);
+
+    socket.on("update-document", (documentId) => {
+        console.log(
+            `New client connected with id '${socket.id}' joined room: ${documentId}`
+        );
+        socket.join(documentId);
+    });
+
+    socket.on("send-changes", (delta, documentId) => {
+        if (delta.source !== "user") {
+            return;
+        }
+
+        socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`Client disconnected: ${socket.id}`);
+    });
+});
 
 app.use(cors());
 app.disable("x-powered-by");
@@ -14,7 +45,7 @@ app.disable("x-powered-by");
 // don't show the log when it is test
 if (process.env.NODE_ENV !== "test") {
     // use morgan to log at command line
-    app.use(morgan("combined")); // 'combined' outputs the Apache style LOGs
+    app.use(morgan("dev")); // 'combined' outputs the Apache style LOGs
 }
 
 // parse application/json
@@ -32,6 +63,8 @@ app.use((req, res, next) => {
     next();
 });
 
-const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+const server = httpServer.listen(port, () =>
+    console.log(`Listening on port ${port}`)
+);
 
 module.exports = server;
