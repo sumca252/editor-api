b41d3d5e61082d354ab2bf2007d7b8a7bd5a6cc4
sumca252 <ismail.ali9315@gmail.com>
2022-09-27 22:45:55 +0200

    adds login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,9 +3,12 @@
  */
 const router = require("express").Router();
 
-const { registerUser } = require("../controllers/auth.controller");
+const { registerUser, loginUser } = require("../controllers/auth.controller");
 
 // POST /auth/register - register a new user
 router.post("/register", registerUser);
+
+// POST /api/auth/login - Login a user
+router.post("/login", loginUser);
 
 module.exports = router;
