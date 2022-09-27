e11af854c125cfda223a47d37f4420e82352c9e2
sumca252 <ismail.ali9315@gmail.com>
2022-09-27 22:45:44 +0200

    adds login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@
  * Authentication controller
  */
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const database = require("../db/database");
 const usersCollection = "users";
@@ -50,6 +51,51 @@ const authController = {
         } catch (error) {
             console.log(error);
             return res.status(500).json({ message: "Something went wrong" });
+        } finally {
+            await db.client.close();
+        }
+    },
+    loginUser: async (req, res) => {
+        try {
+            const { email, password } = req.body;
+
+            if (!email || !password) {
+                return res
+                    .status(400)
+                    .json({ message: "Missing email or password" });
+            }
+
+            db = await database.getDb(usersCollection);
+            const user = await db.collection.findOne({ email });
+
+            if (!user) {
+                return res
+                    .status(400)
+                    .json({ status: 400, message: "User does not exist" });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res
+                    .status(400)
+                    .json({ status: 400, message: "Invalid password!" });
+            }
+
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+                expiresIn: "24h",
+            });
+
+            return res.status(200).json({
+                status: 200,
+                user: {
+                    id: user._id,
+                    email: user.email,
+                    token: token,
+                },
+            });
+        } catch (err) {
+            return res.status(500).json({ message: err.message });
         } finally {
             await db.client.close();
         }
