56899ca2c54e01d605da20ad6880536d7d88d964
sumca252 <ismail.ali9315@gmail.com>
2022-09-27 22:52:22 +0200

    adds reset controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -30,6 +30,32 @@ const usersController = {
             await db.client.close();
         }
     },
+    resetUsersCollection: async (req, res) => {
+        try {
+            db = await database.getDb(usersCollection);
+            await db.collection.deleteMany({});
+
+            // insert many and return inserted documents
+            const result = await db.collection.insertMany(defaultUsers);
+
+            if (result.deletedCount === 0) {
+                return res.status(400).json({
+                    message: "No users found",
+                });
+            }
+
+            return res.status(200).json({
+                insertedIds: result.insertedIds,
+                message: "Users collection reset",
+            });
+        } catch (error) {
+            return res.status(500).json({
+                message: error.message,
+            });
+        } finally {
+            await db.client.close();
+        }
+    },
 };
 
 module.exports = usersController;
