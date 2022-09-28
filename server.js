b1b86c101265479525e29b111f1ae112677ad9a1
sumca252 <ismail.ali9315@gmail.com>
2022-09-28 14:38:11 +0200

    sets cors  methods

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
         origin: "*",
-        methods: ["GET", "POST"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
 );
